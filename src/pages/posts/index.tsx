51398fdfb1a0200aac985f2fc17fa3949edb65e4
Nadilson <nadilson@protonmail.com>
2022-07-02 21:59:06 -0300

    Consumindo posts direto do CMS remoto (Prismic)

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -9,8 +9,30 @@ import {
   FiChevronRight,
   FiChevronsRight,
 } from "react-icons/fi";
+import { getPrismicClient } from "./../../services/prismic";
+import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
+import { GetStaticProps } from "next";
+import { useState } from "react";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  cover: string;
+  description: string;
+  updatedAt: string;
+};
+interface PostsProps {
+  posts: Post[];
+}
+
+// https://png-pixel.com/
+
+export default function Posts({ posts: postsBlog }: PostsProps) {
+  // console.log(posts);
+
+  const [posts, setPosts] = useState(postsBlog || []);
+
   return (
     <>
       <Head>
@@ -18,25 +40,24 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <Link href="/">
-            <a>
-              <Image
-                src={thumbImg}
-                alt="Post título 1"
-                width={720}
-                height={410}
-                quality={100}
-              />
-              <strong>Criando meu primeiro aplicativo</strong>
-              <time>30 de Junho de 2022</time>
-              <p>
-                Hoje, vamos criar o controle de mostrar a senha no input, uma
-                opção para os nossos formulários de cadastro e login. Mas, chega
-                de conversa e bora pro código junto comigo que o vídeo está show
-                de bola!
-              </p>
-            </a>
-          </Link>
+          {posts.map((post) => (
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a key={post.slug}>
+                <Image
+                  src={post.cover}
+                  alt={post.title}
+                  width={720}
+                  height={410}
+                  quality={100}
+                  placeholder="blur"
+                  blurDataURL="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPkbGqsBwADNQGNP8FghAAAAABJRU5ErkJggg=="
+                />
+                <strong>{post.title}</strong>
+                <time>{post.updatedAt}</time>
+                <p>{post.description}</p>
+              </a>
+            </Link>
+          ))}
 
           <div className={styles.buttonNavigate}>
             <div>
@@ -64,3 +85,42 @@ export default function Posts() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.Predicates.at("document.type", "post")],
+    {
+      orderings: "[document.last_publication_date desc]",
+      fetch: ["post.title", "post.description", "post.cover"],
+      pageSize: 3,
+    }
+  );
+
+  // console.log(JSON.stringify(response, null, 2));
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      description:
+        post.data.description.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      cover: post.data.cover.url,
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
+
+  return {
+    props: { posts },
+  };
+  revalidate: 60 * 30; // Atualiza a cada 30 minutos.
+};
